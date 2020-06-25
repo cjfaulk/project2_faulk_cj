9c1504616d6c529d41e83552dca8f18425dffb85
CJ Faulk <cjfaulk@ufl.edu>
2020-06-25 16:46:37 -0400

    Enabled Slick plugin on index.html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.etsy-slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+  });
+});
+
+// $('.etsy-slider').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
